e982c027a39a012320ef7962e99ae12a1843e65c
Subash Singh Dhami <subashdhamee@gmail.com>
2026-04-05 20:23:04 +0545

    backend initiation

---
+++ backend/src/main/java/com/bytestream/ByteStreamApplication.java
@@ -0,0 +1,13 @@
+package com.bytestream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByteStreamApplication.class, args);
+    }
+
+}
